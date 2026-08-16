abbd5eafd8233c0e53c02a682e0e85becd9b1ef0
Narendra Kumar <56841745+Narendra232@users.noreply.github.com>
2026-08-17 02:30:36 +0530

    style: format terraform configuration

--- azureresourses/vms/variables.tf
+++ azureresourses/vms/variables.tf
@@ -5,7 +5,7 @@ variable "vms" {
     resource_group_name = string
     size                = string
     admin_username      = string
-    # CKV_AZURE_1 / CKV_AZURE_149 / CKV_AZURE_178 - SSH public key (no password auth)
-    ssh_public_key = string
+    nic_name            = string
+    ssh_public_key      = string
   }))
 }
