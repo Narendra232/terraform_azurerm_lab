601c715157a95a76a9eb544f03a6637c6769895e
Narendra Kumar <56841745+Narendra232@users.noreply.github.com>
2026-08-17 01:01:14 +0530

    style: format terraform configuration

--- Enviroment/dev/variables.tf
+++ Enviroment/dev/variables.tf
@@ -1,9 +1,32 @@
 
-variable "rgs" {}
-variable "stg" {}
-variable "cnt" {}
-variable "vnets" {}
-variable "subnets" {}
-variable "ips" {}
-variable "nics" {}
-variable "vms" {}
+variable "rgs" {
+  type = any
+}
+
+variable "stg" {
+  type = any
+}
+
+variable "cnt" {
+  type = any
+}
+
+variable "vnets" {
+  type = any
+}
+
+variable "subnets" {
+  type = any
+}
+
+variable "ips" {
+  type = any
+}
+
+variable "nics" {
+  type = any
+}
+
+variable "vms" {
+  type = any
+}
